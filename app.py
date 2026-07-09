ec6073dcd6c1d2ed8cb1c916c7f5c6d9f2949338
abhinayasri26 <abhinayasrikarne@gmail.com>
2026-07-09 09:47:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from google import genai
+import uvicorn
+import os
+# Gemini Client
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+
+# FastAPI App
+app = FastAPI()
+
+# Request Model
+class ChatRequest(BaseModel):
+    message: str
+
+# Home Endpoint
+@app.get("/")
+def home():
+    return {"message": "Gemini AI Backend is Running"}
+
+# Chat Endpoint
+@app.post("/chat")
+def chat(request: ChatRequest):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=request.message
+    )
+
+    return {
+        "question": request.message,
+        "answer": response.text
+    }
+
